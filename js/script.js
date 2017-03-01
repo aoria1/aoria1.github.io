ef272047eba19ba41c203bd27d698f280e01bab1
aoria1 <aoria1@live.com>
2017-02-28 22:18:00 -0600

    added overlay

---
+++ js/script.js
@@ -0,0 +1,15 @@
+function openNav() {
+    document.getElementById("jipa").style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById("jipa").style.width = "0%";
+}
+
+function openNavT() {
+    document.getElementById("truevalue").style.width = "100%";
+}
+
+function closeNavT() {
+    document.getElementById("truevalue").style.width = "0%";
+}
