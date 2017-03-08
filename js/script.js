2e045a2a520496b92ce30aceaa4eb7203ec3f3ff
aoria1 <aoria1@live.com>
2017-03-07 22:33:38 -0600

    responsive carsoual

--- js/script.js
+++ js/script.js
@@ -58,3 +58,43 @@ $(document).ready(function(){
         $("#myCarousel").carousel("next");
     });
 });
+
+$(document).ready(function(){
+    // Activate Carousel
+   $("#myCarousel2").carousel({interval: false});
+    
+    
+    // Enable Carousel Indicators
+    $(".item1").click(function(){
+        $("#myCarousel2").carousel(0);
+    });
+    $(".item2").click(function(){
+        $("#myCarousel2").carousel(1);
+    });
+    $(".item3").click(function(){
+        $("#myCarousel2").carousel(2);
+    });
+    $(".item4").click(function(){
+        $("#myCarousel2").carousel(3);
+    });
+     $(".item5").click(function(){
+        $("#myCarousel2").carousel(4);
+    });
+	 $(".item6").click(function(){
+        $("#myCarousel2").carousel(5);
+    });
+	 $(".item7").click(function(){
+        $("#myCarousel2").carousel(6);
+    });
+	 $(".item8").click(function(){
+        $("#myCarousel2").carousel(7);
+    });
+    // Enable Carousel Controls
+    $(".left").click(function(){
+        $("#myCarousel2").carousel("prev");
+    });
+    $(".right").click(function(){
+        $("#myCarousel2").carousel("next");
+    });
+});
+
